e0a477d3aea54c447cd4c334f99c79d6373ac246
TonyUCLA <TonyUCLA>
2016-10-26 23:09:56 -0700

    timer working

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,19 +1,23 @@
 
 // Once the user pushes start, timer will begin.
-var number = 10
 var correctAnswers = 0;
 var wrongAnswers = 0;
 var unAnswered = 0;
 
 $("#startGame").on("click", runGame);
 
+function startTimer() {
+	number = 10;
+	counter = setInterval(countDown, 1000);
+}
 
-function decrement() {
+function countDown() {
 	number--;
 	$("#runTimer").html("<h2>" + number + "</h2>")
-
 	if (number === 0){
 		stop();
+	$("#runTimer").html("<h2> Times Up! </h2>")
+	unAnswered++;
 	}
 }
 
@@ -48,13 +52,15 @@ var questions = {
 // -----Game Start------
 function runGame() {
 
-	counter = setInterval(decrement, 1000);
+for (var i = 0; i < questions.length; i++) {
+	startTimer();
 
-	$("#question").html(questions.firstSet.question);
-	$("#answerOne").html(questions.firstSet.answers[0]);
-	$("#answerTwo").html(questions.firstSet.answers[1]);
-	$("#answerThree").html(questions.firstSet.answers[2]);
-	$("#answerFour").html(questions.firstSet.answers[3]);
+		$("#question").html(questions.firstSet.question);
+		$("#answerOne").html(questions.firstSet.answers[0]);
+		$("#answerTwo").html(questions.firstSet.answers[1]);
+		$("#answerThree").html(questions.firstSet.answers[2]);
+		$("#answerFour").html(questions.firstSet.answers[3]);
+}
 
 $("#answerOne").click(function(){
 var answerOne = false;
@@ -81,14 +87,11 @@ $("#answerFour").click(function(){
 	$(".content").html("Wrong Answer!"); // display what the correct answer should be.
 	console.log(questions.firstSet.answers[0]);
 })
-setTimeout(secondSet, 3000);
+
 }
 // ----- Second Set Begins ----
 // maybe use a for loop?
 function secondSet() {
-	counter = setInterval(decrement, 1000);
-
-
 
 		$("#question").html(questions.firstSet.question);
 		$("#answerOne").html(questions.firstSet.answers[0]);
