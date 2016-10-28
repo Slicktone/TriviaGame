37995d98e78290d913933afe1ea331deba4c6d4c
TonyUCLA <TonyUCLA>
2016-10-27 18:33:53 -0700

    Almost

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -25,103 +25,120 @@ function stop() {
 	clearInterval(counter);
 }
 
-// questions and answer bank
-var questions = {
-	firstSet: {
-		question : "What is the name of the first Harry Potter Book?",
-		answers : ["The Sorcerers Stone", "The Two Towers", "The Flight of the Phoenix", "The Prisoner of Azcaban"],
-		picture : []
-	},
-	secondSet: {
-		question : "test234",
-		answers : ["firstAnswer1", "secondAnswer2", "thirdAnswer3", "fourthAnswer4"],
-		picture : []
-	},
-	thirdSet: {
-		question : "test567",
-		answers : ["firstAnswer0", "secondAnswer0", "thirdAnswer0", "fourthAnswer0"],
-		picture : []
-	},
-	fourthSet: {
-		question : "test890",
-		answers : ["1firstAnswer", "2secondAnswer", "3thirdAnswer", "4fourthAnswer"],
-		picture : []
-	}
+function startNextTime() {
+	console.log("top");
+	// SetTimeout function goes here, set to 3 seconds.
+	currentQuestion++;
+	runGame(questionSet[currentQuestion]);
+	console.log("bottom");
 }
+
+// questions and answer bank
+// var questions = {
+// 	firstSet: {
+// 		question : "What is the name of the first Harry Potter Book?",
+// 		answers : ["The Sorcerers Stone", "The Two Towers", "The Flight of the Phoenix", "The Prisoner of Azcaban"],
+// 		picture : []
+// 	},
+// 	secondSet: {
+// 		question : "test234",
+// 		answers : ["firstAnswer1", "secondAnswer2", "thirdAnswer3", "fourthAnswer4"],
+// 		picture : []
+// 	},
+// 	thirdSet: {
+// 		question : "test567",
+// 		answers : ["firstAnswer0", "secondAnswer0", "thirdAnswer0", "fourthAnswer0"],
+// 		picture : []
+// 	},
+// 	fourthSet: {
+// 		question : "test890",
+// 		answers : ["1firstAnswer", "2secondAnswer", "3thirdAnswer", "4fourthAnswer"],
+// 		picture : []
+// 	}
+// }
 // I THINK THIS IS THE DIRECTION I WANT TO GO TO LOOPS
 // Use this array of objects to iterate through each question?
- /* var questionSet = [
-{firstSet: {
+  
+  var currentQuestion = 0;
+  var questionSet = [
+ {
 	question : "test123",
 	answers: ["answer1", "answer2", "answer3"],
 	picture : []
-	}
- secondSet: {
+},
+	
+{
  	question : "test123",
- 	answer: ["answer1", "answer2", "answerThree"],
+ 	answers: ["answer1", "answer2", "answerThree"],
  	picture : []
  }
-}];
-*/
+
+];
+
 
 
 // -----Game Start------
-function runGame() {
+function runGame(questionSet) {
 
 // for (var i = 0; i < questions.length; i++) {
 	startTimer();
 
-		$("#question").html(questions.firstSet.question);
-		$("#answerOne").html(questions.firstSet.answers[0]);
-		$("#answerTwo").html(questions.firstSet.answers[1]);
-		$("#answerThree").html(questions.firstSet.answers[2]);
-		$("#answerFour").html(questions.firstSet.answers[3]);
+		$("#question").html(questionSet.question);
+		$("#answerOne").html(questionSet.answers[0]);
+		$("#answerTwo").html(questionSet.answers[1]);
+		$("#answerThree").html(questionSet.answers[2]);
+		$("#answerFour").html(questionSet.answers[3]);
 // }
 
 $("#answerOne").click(function(){
 			correctAnswers++;
-			$(".content").html(""); // displays correct answer.
-			
+			$(".content").html("Correct!"); // displays correct answer.
+			startNextTime();
 		})
-
 // Testing out these click functions on the buttons ******SHORTEN THESE LATER*****
 $("#answerTwo").click(function(){
 	incorrect++
-	$(".content").html("Wrong Answer!"); // display what the correct answer should be.
-	console.log(questions.firstSet.answers[0]);
+	$(".content").html("Wrong Answer!");
+	startNextTime(); // display what the correct answer should be.
+	console.log(questionSet.answers[0]);
 })
 $("#answerThree").click(function(){
 	incorrect++
-	$(".content").html("Wrong Answer!"); // display what the correct answer should be.
-	console.log(questions.firstSet.answers[0]);
+	$(".content").html("Wrong Answer!");
+	startNextTime(); // display what the correct answer should be.
+	console.log(questionSet.answers[0]);
 })
 $("#answerFour").click(function(){
 	incorrect++
-	$(".content").html("Wrong Answer!"); // display what the correct answer should be.
-	console.log(questions.firstSet.answers[0]);
+	$(".content").html("Wrong Answer!");
+	startNextTime(); // display what the correct answer should be.
+	console.log(questionSet.answers[0]);
 })
 
 }
-// ----- Second Set Begins ----
-// maybe use a for loop?
-function secondSet() {
 
-		$("#question").html(questions.firstSet.question);
-		$("#answerOne").html(questions.firstSet.answers[0]);
-		$("#answerTwo").html(questions.firstSet.answers[1]);
-		$("#answerThree").html(questions.firstSet.answers[2]);
-		$("#answerFour").html(questions.firstSet.answers[3]);
+runGame(questionSet[currentQuestion])
 
-$("#answerOne").click(function(){
-var answerOne = false;
-	if(!answerOne) {
-			answerOne = true;
-			correctAnswers++;
-			$(".content").html("Correct!"); // display correct answer.
-			setTimeout(thirdSet, 3000);
-			}
-})		
-}		
+
+
+
+
+// 		$("#question").html(questions.firstSet.question);
+// 		$("#answerOne").html(questions.firstSet.answers[0]);
+// 		$("#answerTwo").html(questions.firstSet.answers[1]);
+// 		$("#answerThree").html(questions.firstSet.answers[2]);
+// 		$("#answerFour").html(questions.firstSet.answers[3]);
+
+// $("#answerOne").click(function(){
+// var answerOne = false;
+// 	if(!answerOne) {
+// 			answerOne = true;
+// 			correctAnswers++;
+// 			$(".content").html("Correct!"); // display correct answer.
+// 			setTimeout(thirdSet, 3000);
+// 			}
+// })		
+// }		
 
 
 
